15c23fe040ac5a272fee67b004cc56cd5529a6b2
bhawnapandit26 <bhawnapandit26@gmail.com>
2026-04-25 12:31:02 +0530

    Mobile layout header size reduced

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return(
-        <div className="pt-7 pb-1 md:pb-3">
-            <h1 className="text-center text-4xl font-bold ">Job Application Tracker</h1>
+        <div className="pt-7 pb-0 md:pb-3">
+            <h1 className="text-center sm:text-4xl text-2xl font-bold ">Job Application Tracker</h1>
             <p className="text-center text-sm pt-1">A place where you can keep record of all your applications. </p>
         </div>
     )
